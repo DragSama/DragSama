b179e19ba9b74e0f1aa4206dc81a1be04b1aaf38
DragSama <49992845+DragSama@users.noreply.github.com>
2020-08-05 13:26:33 +0530

    Turns out AniList uses British English

--- update_fav.py
+++ update_fav.py
@@ -22,10 +22,10 @@ regex = re.compile("<!-- anime_list_start-->.*<!-- anime_list_end-->", re.DOTALL
 
 def update_readme():
   vars = {"query": 'DragSama'}
-  req = requests.post('https://graphql.anilist.co', json={'query': query, 'variables': vars}).json()['data']['User']['favorites']['anime']['nodes']
+  req = requests.post('https://graphql.anilist.co', json={'query': query, 'variables': vars}).json()['data']['User']['favourites']['anime']['nodes']
   new = '<!-- anime_list_start-->\n'
   for x in req:
-    new += f"* {x['title']['english']}({x['title']['romaji']})\n"
+    new += f"* {x['title']['english']} - ({x['title']['romaji']})\n"
   new += "<!-- anime_list_end-->"
   with open('readme.md', 'r') as f:
     text = regex.sub(new, f.read())
