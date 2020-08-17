0659ccea2a1aa86e335ddb919918163c7a4dc879
DragSama <49992845+DragSama@users.noreply.github.com>
2020-08-17 10:23:26 +0530

    Change query, Make anime list link to anime's anilist page

--- update_fav.py
+++ update_fav.py
@@ -7,6 +7,7 @@ query($query: String){
     favourites{
       anime {
         nodes{
+          siteUrl
           title {
             romaji
             english
@@ -25,7 +26,7 @@ def update_readme():
   req = requests.post('https://graphql.anilist.co', json={'query': query, 'variables': vars}).json()['data']['User']['favourites']['anime']['nodes']
   new = '<!-- anime_list_start-->\n'
   for x in req:
-    new += f"* {x['title']['english']} - ({x['title']['romaji']})\n"
+    new += f"* [{x['title']['english']}]({x['siteUrl']}) - ({x['title']['romaji']})\n"
   new += "<!-- anime_list_end-->"
   with open('README.md', 'r') as f:
     text = regex.sub(new, f.read())
